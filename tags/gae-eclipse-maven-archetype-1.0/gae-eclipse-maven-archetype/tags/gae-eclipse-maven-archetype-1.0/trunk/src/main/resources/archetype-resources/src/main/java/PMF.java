8b56cc715f2ce6a97265cf2a384aeb1253249d93
Hannu Leinonen <hleinone@gmail.com>
2010-01-25 22:45:13 +0000

    [maven-scm] copy for tag gae-eclipse-maven-archetype-1.0

---
+++ tags/gae-eclipse-maven-archetype-1.0/gae-eclipse-maven-archetype/tags/gae-eclipse-maven-archetype-1.0/trunk/src/main/resources/archetype-resources/src/main/java/PMF.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${packageInPathFormat};
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+public final class PMF {
+	private static final PersistenceManagerFactory pmfInstance = JDOHelper
+			.getPersistenceManagerFactory("transactions-optional");
+
+	private PMF() {
+	}
+
+	public static PersistenceManagerFactory get() {
+		return pmfInstance;
+	}
+}
